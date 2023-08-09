fa814f1c6909da244740e4d81eff91135ff1ad81
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-08-09 23:33:21 +0000

    [lab5] Completed exercise 3

--- fs/fs.c
+++ fs/fs.c
@@ -65,7 +65,14 @@ alloc_block(void)
 	// super->s_nblocks blocks in the disk altogether.
 
 	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	uint32_t blockno;
+	for (blockno = 0; blockno < super->s_nblocks; blockno++) {
+		if (block_is_free(blockno)) {
+			bitmap[blockno / 32] &= ~(1 << (blockno % 32));
+			flush_block(bitmap + blockno / 32);
+			return blockno;
+		}
+	}
 	return -E_NO_DISK;
 }
 
